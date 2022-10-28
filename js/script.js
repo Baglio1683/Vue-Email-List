70d1c031b2cb0a879b39c91c4d15d0bbc4af0e55
MyUsername <alberto.baglivi@gmail.com>
2022-10-28 16:09:33 +0200

    add the program

--- js/script.js
+++ js/script.js
@@ -5,7 +5,25 @@ createApp({
 data(){
 
     return{
-        message :  "hello"
+        emailName : "", 
+        arrayNum : [], 
     }
-}
+}, 
+
+created(){
+
+    for(let i =0 ; i < 10 ; i++){
+
+            axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((resp)=>{
+            console.log(resp)
+            this.emailName = resp.data.response
+            this.arrayNum.push(this.emailName)
+            this.emailName = ""
+            });
+          }
+        } 
+
+
+    
+
 }).mount("#app");
